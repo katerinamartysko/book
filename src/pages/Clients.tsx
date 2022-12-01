8497bc0cc5f0d7fcf11b564eca6d13c00677afb8
Katerina <katerinamartysko@gmail.com>
2022-12-01 23:47:48 +0200

    Fetch and save clients from the backend

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,8 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Breadcrumbs, Divider, List, ListItem, ListItemText, ListSubheader } from '@mui/material';
-import '../App.css';
-import { useSelector } from 'react-redux';
+import { getClients } from '../store/clients/actions';
+import { useFetching } from '../hooks/useFetching';
+import PostService from '../api/PostServise';
 import { State } from '../store/store';
+import '../App.css';
 
 const style = {
   width: '100%',
@@ -11,7 +14,17 @@ const style = {
 };
 
 const Clients = () => {
+  const dispatch = useDispatch();
   const clients = useSelector((state: State) => state.clients.clients);
+
+  const [fetchClients] = useFetching(async () => {
+    const response = await PostService.getClients();
+    dispatch(getClients(response.data));
+  });
+
+  useEffect(() => {
+    fetchClients();
+  }, []);
 
   return (
     <div className="root">
