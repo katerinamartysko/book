f823a847b712e34740b055c164e6a513928af678
Katerina <katerinamartysko@gmail.com>
2022-12-09 14:34:39 +0200

    add useClients hook for the search logic

---
+++ src/hooks/useClients.ts
@@ -0,0 +1,13 @@
+import { useMemo } from 'react';
+import { ClientList } from '../api/types';
+
+export const useClients = (clientsList: Array<ClientList>, query: string | null): Array<ClientList> => {
+  if (query === null) return clientsList;
+  return useMemo(() => {
+    return clientsList.filter(
+      client =>
+        client.general.firstName.toLowerCase().includes(query.toLowerCase()) ||
+        client.general.lastName.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [query]);
+};
