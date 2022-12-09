49a993231536c2a652518fc0b9151324a75a576b
Katerina <katerinamartysko@gmail.com>
2022-12-09 16:04:26 +0200

    Add logic for search Clients List

--- src/hooks/useClients.ts
+++ src/hooks/useClients.ts
@@ -1,13 +1,10 @@
-import { useMemo } from 'react';
 import { ClientList } from '../api/types';
 
 export const useClients = (clientsList: Array<ClientList>, query: string | null): Array<ClientList> => {
   if (query === null) return clientsList;
-  return useMemo(() => {
-    return clientsList.filter(
-      client =>
-        client.general.firstName.toLowerCase().includes(query.toLowerCase()) ||
-        client.general.lastName.toLowerCase().includes(query.toLowerCase())
-    );
-  }, [query]);
+  return clientsList.filter(
+    client =>
+      client.general.firstName.toLowerCase().includes(query.toLowerCase()) ||
+      client.general.lastName.toLowerCase().includes(query.toLowerCase())
+  );
 };
