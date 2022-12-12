d4cfcb9816fd89d08db622242a53722486c71812
Katerina <katerinamartysko@gmail.com>
2022-12-12 14:07:11 +0200

    add action and reducer for search

--- src/store/clients/actions.ts
+++ src/store/clients/actions.ts
@@ -1,9 +1,10 @@
 import { Client, ClientList } from '../../api/types';
 import { PayloadAction } from '../types';
-import c from '../clients/constants';
+import c from '../constants';
 
 export type GetClientsAction = PayloadAction<c.GET_CLIENTS_LIST, Array<ClientList>>;
 export type GetClientsIdAction = PayloadAction<c.GET_CLIENT_ID, Client>;
+export type Search = PayloadAction<c.SEARCH, string | null>;
 
 export const getClientsList = (clientsList: Array<ClientList>): GetClientsAction => ({
   type: c.GET_CLIENTS_LIST,
@@ -13,4 +14,8 @@ export const getClientsList = (clientsList: Array<ClientList>): GetClientsAction
 export const getClientId = (client: Client): GetClientsIdAction => ({
   type: c.GET_CLIENT_ID,
   payload: client,
+});
+export const setSearch = (query: string): Search => ({
+  type: c.SEARCH,
+  payload: query,
 });
