8f4e3317c79e3814d02a44bdcd2f900f0df59db4
Katerina <katerinamartysko@gmail.com>
2022-12-12 14:39:39 +0200

    change search logic and use selectors for the clients and search

--- src/components/ClientsList.tsx
+++ src/components/ClientsList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from 'tss-react/mui';
 import {
@@ -11,10 +11,10 @@ import {
   ListItemText,
   ListSubheader,
 } from '@mui/material';
-import { useClients, useFetching } from '../hooks';
+import { selectClientsList, selectSearch } from '../store/clients/selectors';
+import { getClientsList, setSearch } from '../store/clients/actions';
 import PostService from '../api/PostServise';
-import { getClientsList } from '../store/clients/actions';
-import { State } from '../store/store';
+import { useFetching } from '../hooks';
 import { theme } from '../utils/them';
 import { Search } from './Search';
 
@@ -43,10 +43,8 @@ export const ClientsList: FC<Props> = ({ setClientID }) => {
   const dispatch = useDispatch();
   const { classes } = useStyles();
 
-  const clients = useSelector((state: State) => state.clients.clientsList);
-  const [search, setSearch] = useState<string | null>(null);
-
-  const searchClients = useClients(clients, search);
+  const clients = useSelector(selectClientsList);
+  const search = useSelector(selectSearch);
 
   const [fetchClients, isClientsLoading, clientError] = useFetching(async () => {
     const response = await PostService.getClients();
@@ -57,12 +55,16 @@ export const ClientsList: FC<Props> = ({ setClientID }) => {
     fetchClients();
   }, []);
 
+  const handelSearch = (search: string): void => {
+    dispatch(setSearch(search));
+  };
+
   if (isClientsLoading) return <CircularProgress className={classes.load} />;
   if (clientError) return <h1 className="error">Произошла ошибка {clientError}</h1>;
   if (!clients.length) return <h1> No clients found </h1>;
   return (
     <div>
-      <Search search={search} setSearch={setSearch} />
+      <Search search={search} onSearch={handelSearch} />
       <div>
         <Breadcrumbs aria-label="breadcrumb">
           <List
@@ -70,7 +72,7 @@ export const ClientsList: FC<Props> = ({ setClientID }) => {
             aria-label="mailbox folders"
             subheader={<ListSubheader component="div">Phone book</ListSubheader>}
           >
-            {searchClients.map(client => (
+            {clients.map(client => (
               <div key={client.id}>
                 <div className={classes.list}>
                   <Avatar alt="Remy Sharp" src={client.general.avatar} className={classes.avatar} />
