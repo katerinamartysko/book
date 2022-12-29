042aaf53edec999a292b74af336743503a0eec3e
Katerina <katerinamartysko@gmail.com>
2022-12-29 12:22:54 +0200

    use useDebouncedCallback

--- src/components/ClientsListContainer.tsx
+++ src/components/ClientsListContainer.tsx
@@ -1,13 +1,13 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from 'tss-react/mui';
 import { CircularProgress } from '@mui/material';
 import { selectClientsList, selectSearch } from '../store/clients/selectors';
 import { getClientsList, setSearch } from '../store/clients/actions';
+import { useFetching, useDebouncedCallback } from '../hooks';
 import PostService from '../api/PostServise';
-import { useFetching } from '../hooks';
-import { Search } from './Search';
 import { ClientList } from './ClientList';
+import { Search } from './Search';
 
 const useStyles = makeStyles()(() => ({
   root: {
@@ -29,6 +29,7 @@ export const ClientsListContainer: FC<Props> = ({ clientID, onSetClientID }) => 
 
   const clients = useSelector(selectClientsList);
   const search = useSelector(selectSearch);
+  const [clientSearch, setClientSearch] = useState<string | null>(search);
 
   const [fetchClients, isClientsLoading, clientError] = useFetching(async () => {
     const response = await PostService.getClients();
@@ -39,15 +40,20 @@ export const ClientsListContainer: FC<Props> = ({ clientID, onSetClientID }) => 
     fetchClients();
   }, []);
 
-  const handelSearch = (search: string): void => {
+  const searchClients = useDebouncedCallback((search: string): void => {
     dispatch(setSearch(search));
+  }, 800);
+
+  const handelSearch = (search: string): void => {
+    setClientSearch(search);
+    searchClients(search);
   };
 
   if (isClientsLoading) return <CircularProgress className={classes.load} />;
   if (clientError) return <h1 className="error">Произошла ошибка {clientError}</h1>;
   return (
     <div className={classes.root}>
-      <Search search={search} onSearch={handelSearch} />
+      <Search search={clientSearch} onSearch={handelSearch} />
       <ClientList clientID={clientID} clients={clients} onSetClientID={onSetClientID} />
     </div>
   );
