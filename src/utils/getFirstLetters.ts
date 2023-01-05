1a20027cea97b24295bd8717250a093b3047ba22
Katerina <katerinamartysko@gmail.com>
2023-01-05 12:53:17 +0200

    refactor

--- src/utils/getFirstLetters.ts
+++ src/utils/getFirstLetters.ts
@@ -1,6 +1,5 @@
 import { General } from '../api/types';
 
 export const getFirstLetters = (general: General): string => {
-  if (general.firstName) return `${general.firstName[0]}${general.lastName[0]}`;
-  return general.lastName[0];
+  return [general.firstName?.[0], general.lastName[0]].filter(Boolean).join('');
 };
