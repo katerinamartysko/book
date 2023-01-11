e01cb12e451cd879c7e1e24ca1a7f471b57d927f
Katerina <katerinamartysko@gmail.com>
2023-01-11 17:02:25 +0200

    add useClient

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -1,16 +1,12 @@
-import React, { FC, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { FC } from 'react';
 import { makeStyles } from 'tss-react/mui';
 import classNames from 'classnames';
 import { Avatar, Box, CircularProgress, Divider, Typography } from '@mui/material';
-import { getClient, removeClient } from '../store/clients/actions';
-import { selectClient } from '../store/clients/selectors';
 import { theme, getFirstLetters } from '../utils';
-import PostService from '../api/PostServise';
 import { Description } from './Description';
-import { useFetching } from '../hooks';
 import { Section } from './Section';
 import { BackButton } from './index';
+import { useClient } from '../hooks/useClient';
 
 const useStyles = makeStyles()(() => ({
   flex: {
@@ -42,31 +38,29 @@ const useStyles = makeStyles()(() => ({
 }));
 
 interface Props {
-  clientID: number | null;
+  clientID: number;
   onRemoveClientID: () => void;
 }
 
 export const Client: FC<Props> = ({ clientID, onRemoveClientID }) => {
-  const dispatch = useDispatch();
   const { classes } = useStyles();
-  const client = useSelector(selectClient);
+  const { data: client, isLoading: isClientLoading, isError } = useClient(clientID);
 
-  const [fetchClientsById, isClientIdLoading, clientIdError] = useFetching(async (id: number) => {
-    const response = await PostService.getClient(id);
-    dispatch(getClient(response.data));
-  });
-
-  useEffect(() => {
-    if (clientID === null) {
-      dispatch(removeClient(null));
-      return;
-    }
-    fetchClientsById(clientID);
-  }, [clientID]);
-
-  if (!client) return null;
+  if (!client) return <Typography>No client</Typography>;
   return (
     <>
+      {isClientLoading && (
+        <Box className={classes.flex}>
+          <CircularProgress />
+        </Box>
+      )}
+
+      {isError && (
+        <Typography variant="h1" className="error">
+          Error
+        </Typography>
+      )}
+
       <BackButton onRemoveClientID={onRemoveClientID} />
       <div className={classNames(classes.root, classes.flex)}>
         <div className={classNames(classes.details, classes.flex)}>
@@ -92,17 +86,6 @@ export const Client: FC<Props> = ({ clientID, onRemoveClientID }) => {
             </Section>
           </div>
         </div>
-
-        {clientIdError && (
-          <Typography variant="h1" className="error">
-            Error {clientIdError}
-          </Typography>
-        )}
-        {isClientIdLoading && (
-          <Box className={classes.flex}>
-            <CircularProgress />
-          </Box>
-        )}
       </div>
     </>
   );
